23a328bddf2b4e159a522d3d6349786bb2e5f120
Aiden Liu <wugunawki@gmail.com>
2024-07-17 21:40:16 -0700

    update google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Theming from "@/Theme/Theming";
+import Script from 'next/script';
 import "./globals.css";
 
 import Headpage from "./Header/page";
@@ -6,6 +7,21 @@ import Headpage from "./Header/page";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body>
         <Theming>
           <Headpage />
